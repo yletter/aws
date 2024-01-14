8e6c467adf8605fd4e6e2348fc916a477ef45662
yuvarajengineer <yuvarajengineer@gmail.com>
2024-01-14 20:20:45 +0530

    Test

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -1,7 +1,7 @@
-package hello;
+package yuv;
 
 public class Greeter {
     public String sayHello() {
-        return "DevOps Project for AchiStar Technologies";
+        return "DevOps Project";
     }
 }
